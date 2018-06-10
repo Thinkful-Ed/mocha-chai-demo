13ecec4ed614b2707ae9a31bc7432d53d24676d2
Chris Tsongas <chris.tsongas@gmail.com>
2018-06-10 18:29:12 +0000

    fix syntax error

--- test/test-is-equal.js
+++ test/test-is-equal.js
@@ -27,7 +27,7 @@ describe('isEqual', function() {
         ];
         unequalInputs.forEach(function(input) {
             const answer = isEqual(input[0], input[1]);
-            answer.should.be.false
+            answer.should.be.false;
         });
     });
   }
